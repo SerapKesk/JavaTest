e51604a1d18ef8930653fa9ef49030c4e3f5b0b5
SerapKesk <serap.kurt@gmail.com>
2024-03-25 12:12:28 -0400

    soru 2 ve 3

---
+++ Odev1/src/Soru4.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Soru4 {
+//Bir cümledeki kelimeleri tersten sıralayarak ekrana yazdıran bir Java programı yazın.
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("bir cumle giriniz: ");
+        String sentence= scanner.nextLine();
+
+
+    }
+}
