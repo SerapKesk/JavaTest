58290cdcc460c287c899b692dbc5b42183512234
SerapKesk <serap.kurt@gmail.com>
2024-03-25 09:05:19 -0400

    Odev1

--- Odev1/src/Main.java
+++ Odev1/src/Main.java
@@ -1,5 +1,20 @@
+import jdk.jshell.Snippet;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world! test");
+        Scanner scanner = new Scanner (System.in);
+        System.out.println("cumle girin");
+        String sentence = scanner.nextLine();
+
+        int emptyScape = 0;
+
+        for (int i=0; i< sentence.length(); i++){
+            if(sentence.charAt(i)== ' ') {
+                emptyScape++;
+            }
+        }
+        System.out.println( " cumledeki bosluk sayisi : " + emptyScape);
     }
 }
